7529bc182e1b962c19cc00bb5157ce1ea077de85
GARUD6209 <harshvardhachandravanshi@gmail.com>
2024-12-29 19:14:04 +0530

    docker and nginx

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
-// export const API_URL = "http://localhost:8080";
-export const API_URL = "https://cors-anywhere.herokuapp.com/http://15.207.109.188:8080/auth/signin";
+export const API_URL = "http://localhost:8080";
+// export const API_URL = "http://15.207.109.188:8080/auth/signin";
--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/retail-store-react/',
+  // base: '/retail-store-react/',
   plugins: [react()],
   server: {
     port: 3000
